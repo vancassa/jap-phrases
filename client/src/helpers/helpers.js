dc56bbc8d8d0c8e91113b7cb115541d7003af146
Vanessa <vanessa@pebbleroad.com>
2019-04-25 18:19:28 +0800

    update dist

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -11,7 +11,7 @@ Vue.use(VueFlashMessage, {
 });
 
 const vm = new Vue();
-const baseURL = 'http://localhost:3000/words/';
+const baseURL = 'http://van-todo.herokuapp.com/words/';
 
 const handleError = fn => (...params) =>
   fn(...params).catch(error => {
